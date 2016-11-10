6af75661c94a253d8b501f75c8990fb1e527f6fc
tatsuya-sekine <tatsuya_sekine@fcs.co.jp>
2016-11-10 14:15:53 +0900

    仮詳細画面_関根

--- templates_c/43e13448d506511cced8aab2d29790cd4ae661bd_0.file.next.html.php
+++ templates_c/43e13448d506511cced8aab2d29790cd4ae661bd_0.file.next.html.php
@@ -1,18 +1,18 @@
 <?php
-/* Smarty version 3.1.30, created on 2016-11-07 15:59:28
+/* Smarty version 3.1.30, created on 2016-11-10 06:13:10
   from "C:\xampp\htdocs\Connect2016inKoriyama\templates\next.html" */
 
 /* @var Smarty_Internal_Template $_smarty_tpl */
 if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   'version' => '3.1.30',
-  'unifunc' => 'content_582096d09a9f96_52920363',
+  'unifunc' => 'content_582401e668f410_09959767',
   'has_nocache_code' => false,
   'file_dependency' => 
   array (
     '43e13448d506511cced8aab2d29790cd4ae661bd' => 
     array (
       0 => 'C:\\xampp\\htdocs\\Connect2016inKoriyama\\templates\\next.html',
-      1 => 1478530767,
+      1 => 1478754788,
       2 => 'file',
     ),
   ),
@@ -20,19 +20,45 @@ if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   array (
   ),
 ),false)) {
-function content_582096d09a9f96_52920363 (Smarty_Internal_Template $_smarty_tpl) {
+function content_582401e668f410_09959767 (Smarty_Internal_Template $_smarty_tpl) {
 ?>
 <!DOCTYPE html>
 <html lang="ja">
 	<head>
 		<meta charset="UTF-8">
-		<title>hello</title>
+		<title>イベント詳細画面</title>
 		<?php echo '<script'; ?>
  type="text/javascript" src="./scripts/jquery-3.1.1.min.js"><?php echo '</script'; ?>
 >
+		<?php echo '<script'; ?>
+ type="text/javascript" src="https://maps.googleapis.com/maps/api/js?&key=AIzaSyDS5ci0gvRtGTuR54rtdo7fJ0HtP6lYWJM"><?php echo '</script'; ?>
+>
+		<?php echo '<script'; ?>
+ type="text/javascript" src="./scripts/index.js"><?php echo '</script'; ?>
+>
+		<link rel="stylesheet" href="css\detail.css" type="text/css" media="screen">
 	</head>
 	<body>
-		<ul>
+		<div id="Content">
+			<div id="title"><?php echo $_smarty_tpl->tpl_vars['data']->value[4];?>
+<div ida="kana"><?php echo $_smarty_tpl->tpl_vars['data']->value[5];?>
+</div></div>
+			<div id="img">ここに画像</div>
+			<div id="eventdata"><?php echo $_smarty_tpl->tpl_vars['data']->value[8];?>
+</div>
+			<table>
+				<tr><td>開催地</td><td><?php echo $_smarty_tpl->tpl_vars['data']->value[11];?>
+</td><td>郵便番号</td><td><?php echo $_smarty_tpl->tpl_vars['data']->value[10];?>
+</td></tr>
+				<tr><td>開催日</td><td><?php echo $_smarty_tpl->tpl_vars['data']->value[13];?>
+</td><td>電話番号</td><td><?php echo $_smarty_tpl->tpl_vars['data']->value[9];?>
+</td></tr>
+				<tr><td>開催期間</td><td><?php echo $_smarty_tpl->tpl_vars['data']->value[14];?>
+</td></tr>
+				<tr></tr>
+			</table>
+		</div>
+		<!--<ul>
 			<?php
 $_from = $_smarty_tpl->smarty->ext->_foreach->init($_smarty_tpl, $_smarty_tpl->tpl_vars['data']->value, 'd');
 if ($_from !== null) {
@@ -46,7 +72,10 @@ foreach ($_from as $_smarty_tpl->tpl_vars['d']->value) {
 $_smarty_tpl->smarty->ext->_foreach->restore($_smarty_tpl);
 ?>
 
-		</ul>
+		</ul>-->
+		<div id="Map-Body">
+			<div id="mapField" style="width:700px; height:300px;"></div>
+		</div>
 	</body>
 </html><?php }
 }
